996bb3c7bf640ad650f1ecb0a7617332fb1c158a
yuanli <yuanli@tuzhanai.com>
2020-12-25 14:40:49 +0800

    同一台电脑同时配置gitLab和github

--- src/main/java/com/yl/Application.java
+++ src/main/java/com/yl/Application.java
@@ -4,19 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-
 /**
- * 精准平台--消息队列--生产者
- * @author yuanli 
- * @date 2018年10月31日 上午9:17:23
+ * 同一台电脑同时配置gitLab和github
  */
 @SpringBootApplication
 @ComponentScan(basePackages = "com.yl")
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-		System.out.println("启动了没？");
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+        System.out.println("启动了没？");
+    }
 
 }
